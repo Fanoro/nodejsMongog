dbb60a70ceda99205c7b0cfcce0ba918f874a10a
Fanoro Choque <fanorstalin@gmail.com>
2023-05-26 01:49:55 +0000

    change database

--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,32 +1,32 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const verifyMiddleware = require("../../middlewares/verify.middleware");
-const checkPermissionMiddleware = require("../../middlewares/checkPermission.middleware");
-const userController = require("../../controllers/users/user.controller");
-const adminController = require("../../controllers/users/admin.controller");
+const verifyMiddleware = require('../../middlewares/verify.middleware');
+const checkPermissionMiddleware = require('../../middlewares/checkPermission.middleware');
+const userController = require('../../controllers/users/user.controller');
+const adminController = require('../../controllers/users/admin.controller');
 
 router.post(
-  "/create_user",
+  '/create_user',
   verifyMiddleware,
-  checkPermissionMiddleware(["Gestión de Usuarios"]),
+  checkPermissionMiddleware(['Gestión de Usuarios']),
   userController.createUser
 );
 
-router.get("/getAllu", verifyMiddleware, userController.getAllUsers);
+router.get('/getAllu', verifyMiddleware, userController.getAllUsers);
 
-router.get("/:id", verifyMiddleware, userController.getUser);
+router.get('/:id', verifyMiddleware, userController.getUser);
 
-router.put("/:id", verifyMiddleware, userController.updateUser);
-router.delete("/:id", verifyMiddleware, userController.deactivateUser);
+router.put('/:id', verifyMiddleware, userController.updateUser);
+router.delete('/:id', verifyMiddleware, userController.deactivateUser);
 
-router.patch("/:id/activateu", verifyMiddleware, userController.activateUser);
+router.patch('/:id/activateu', verifyMiddleware, userController.activateUser);
 
 //administrators
 
 router.post(
-  "/create_user_admin",
+  '/create_user_admin',
   verifyMiddleware,
-  checkPermissionMiddleware(["Gestión de Usuarios Administradores"]),
+  checkPermissionMiddleware(['Gestión de Usuarios Administradores']),
   adminController.createAdminUser
 );
 
